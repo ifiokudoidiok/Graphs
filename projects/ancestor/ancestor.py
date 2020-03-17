438dbd2cb671e4d1a2b5a77e8bf502f2534dd7db
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-03-17 16:07:24 +0100

    MVP reached

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,56 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, vertix_id):
+        if vertix_id not in self.vertices:
+            self.vertices[vertix_id] = set()
+    def add_edges(self, v1, v2):
+        if v2 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('That vertex does not exist')
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    #build graph
+    graph = Graph()
+    
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+        # build edges in revers
+        graph.add_edges(pair[1], pair[0])
+    
+    q = Queue()
+    q.enqueue([starting_node])
+
+    max_path_length = 1
+    earliest_ancestor = -1
+
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = v
+            max_path_length = len(path)
+        
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+
+    return earliest_ancestor
